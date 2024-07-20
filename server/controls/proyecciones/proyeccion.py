7fee5b4738490a57f9a137041079ff0064dbbfdf
aSebasISG <140469915+aSebasISG@users.noreply.github.com>
2024-07-20 10:10:38 -0500

    PULL (#24)
    * Prueba de Reports y Notify
    
    ****
    
    * Ajustes Tabla Calificaciones - Eliminación de Clg
    
    Tabla Calificaciones de arreglos a objetos - Console.log  comentados
    
    * Proyecciones
    
    Proyecciones de Parámetros de evaluación por Unidad
    Proyecciones por Unidad1 y Unidad2
    Agregación del Manual de Usuario (Manual para cambio y recuperación de contraseña)
    
    * Proyecciones Supletorios
    
    Agregación de Proyección para supletorios o nota final del estudiante
    
    ---------
    
    Co-authored-by: SebasISG <leoare0412@gmail.com>

--- server/controls/proyecciones/proyeccion.py
+++ server/controls/proyecciones/proyeccion.py
@@ -1,9 +1,66 @@
-
-
-
 import matplotlib.pyplot as plt
 from io import BytesIO
 import colorama
+
+#* ---------------------------------------------------- NOTA FINAL Y NOTA PARA SUPLETORIO ---------------------------------------------------- *
+
+def calcular_y_graficar_proyeccion(nota_unidad1, nota_unidad2, nota_unidad3, nombre_estudiante): 
+    # Verificar si las notas son válidas (entre 0 y 10)
+    if not (0 <= nota_unidad1 <= 10) or not (0 <= nota_unidad2 <= 10) or not (0 <= nota_unidad3 <= 10):
+        raise ValueError("Las notas deben estar en el rango de 0 a 10.")
+
+    # Calcular el promedio de las tres unidades
+    promedio_unidades = (nota_unidad1 + nota_unidad2 + nota_unidad3) / 3
+    
+    if promedio_unidades < 7:
+        # Calcular la nota del 40% y la nota necesaria en el supletorio
+        nota_40 = promedio_unidades * 0.4
+        nota_necesaria_supletorio = (7 - nota_40) / 0.6
+
+        # Datos para el gráfico de proyección supletorio
+        unidades = ['Unidad 1', 'Unidad 2', 'Unidad 3', 'Nota 40%', 'Nota Requerida en Supletorio']
+        notas = [nota_unidad1, nota_unidad2, nota_unidad3, nota_40, nota_necesaria_supletorio]
+        colores = ['green', 'yellow', 'gray', 'blue', 'red']
+        etiquetas = ['Unidad 1', 'Unidad 2', 'Unidad 3', 'Nota 40%', 'Nota Requerida en Supletorio']
+    else:
+        # Datos para el gráfico de proyección final
+        unidades = ['Unidad 1', 'Unidad 2', 'Unidad 3', 'Nota Final']
+        notas = [nota_unidad1, nota_unidad2, nota_unidad3, promedio_unidades]
+        colores = ['green', 'yellow', 'gray', 'blue']
+        etiquetas = ['Unidad 1', 'Unidad 2', 'Unidad 3', 'Nota Final']
+    
+    # Crear el gráfico
+    plt.figure(figsize=(10, 6))
+    bars = plt.bar(unidades, notas, color=colores)
+    
+    # Añadir etiquetas de valor sobre cada barra
+    for bar in bars:
+        yval = bar.get_height()
+        plt.text(bar.get_x() + bar.get_width()/2, yval, round(yval, 2), va='bottom' if yval >= 0 else 'top')
+
+    # Línea horizontal de referencia para la nota mínima para pasar
+    plt.axhline(y=7, color='gray', linestyle='--', label='Nota mínima para pasar')
+
+    # Añadir leyenda para explicar qué representa cada barra
+    handles = [plt.Rectangle((0,0),1,1, color=color) for color in colores]
+    plt.legend(handles, etiquetas, loc='best')
+
+    plt.title(f'Proyección de Nota Final del Estudiante: {nombre_estudiante}')
+    plt.xlabel('Evaluaciones')
+    plt.ylabel('Nota')
+    plt.grid(True)
+    plt.tight_layout()
+
+    # Guardar la imagen en un objeto BytesIO en lugar de mostrarla
+    buf = BytesIO()
+    plt.savefig(buf, format='png')
+    buf.seek(0)
+    
+    return buf
+
+def llamar_calcular_proyeccion_final_supletorio(nota_unidad_1, nota_unidad_2, nota_unidad_3, nombre_estudiante):
+    imagen_grafica = calcular_y_graficar_proyeccion(nota_unidad_1, nota_unidad_2, nota_unidad_3, nombre_estudiante)
+    return imagen_grafica
 
 #* ---------------------------------------------------- UNIDAD 3 ---------------------------------------------------- *
 def calcular_proyeccion_estudiante_unidad_3(nota_unidad1, nota_unidad2):
@@ -37,7 +94,7 @@ def calcular_proyeccion_estudiante_unidad_3(nota_unidad1, nota_unidad2):
     # Retornar la proyección, la nota necesaria para la unidad 3, la suma acumulada, el color y el mensaje
     return proyeccion, nota_necesaria_unidad3, suma_acumulada, color, mensaje
 
-def grafica_proyeccion_estudiante_unidad_3(nota_unidad1, nota_unidad2, proyeccion, nota_necesaria_unidad3, suma_acumulada, color, mensaje):
+def grafica_proyeccion_estudiante_unidad_3(nota_unidad1, nota_unidad2, proyeccion, nota_necesaria_unidad3, suma_acumulada, color, mensaje, nombre_estudiante):
     # Crear gráfico de proyección
     unidades = ['Unidad 1', 'Unidad 2', 'Unidad 3 (Proyección)']
     notas = [nota_unidad1, nota_unidad2, nota_necesaria_unidad3]
@@ -58,7 +115,7 @@ def grafica_proyeccion_estudiante_unidad_3(nota_unidad1, nota_unidad2, proyeccio
     # Mostrar la nota necesaria para pasar como una anotación sobre la línea de referencia
     plt.text(1.8, 7.1, f'Nota necesaria: {nota_necesaria_unidad3}', color='gray', fontsize=10, va='center')
 
-    plt.title('Proyección de Nota Final del Estudiante')
+    plt.title(f'Proyección de Nota Final del Estudiante: {nombre_estudiante}')
     plt.xlabel('Unidades')
     plt.ylabel('Nota')
     plt.legend()
@@ -280,20 +337,70 @@ def graficar_proyecciones_all_estudiantes_parametros(data):
     imagen = calcular_proyeccion_parametros_all_estudaintes(data)
     return imagen
 
-# def main():
-#     # Datos de ejemplo (notas de las unidades)
-#     nota_unidad1 = 8
-#     nota_unidad2 = 8 
-
-#     # Calcular la proyección, la nota necesaria para la unidad 3, la suma acumulada, el color y el mensaje
-#     proyeccion, nota_necesaria_unidad3, suma_acumulada, color, mensaje = calcular_proyeccion_estudiante_unidad_3(nota_unidad1, nota_unidad2)
-
-#     # Graficar la proyección y guardar la imagen en una variable
-#     imagen = grafica_proyeccion_estudiante_unidad_3(nota_unidad1, nota_unidad2, proyeccion, nota_necesaria_unidad3, suma_acumulada, color, mensaje)
+#* ---------------------------------------------------- PROYECCION ESTUDIANTE SUPERLETORIO O FINAL ---------------------------------------------------- *
+def calcular_y_graficar_proyeccion_estudiantes_final_supletorio(data):
+    nombres = []
+    notas_unidades = []
+    notas_finales = []
+    colores_finales = 'blue'  # Usar un solo color para la nota final
     
-#     # Guardar la imagen en un archivo para verificar (opcional)
-#     # with open('proyeccion.png', 'wb') as f:
-#     #     f.write(imagen.getbuffer())
+    for persona in data:
+        nombre = persona['nombre']
+        unidad1 = persona['unidad_1']
+        unidad2 = persona['unidad_2']
+        unidad3 = persona['unidad_3']
+        
+        # Verificar si las notas son válidas (entre 0 y 10)
+        if not (0 <= unidad1 <= 10) or not (0 <= unidad2 <= 10) or not (0 <= unidad3 <= 10):
+            raise ValueError("Las notas deben estar en el rango de 0 a 10.")
 
-# if __name__ == "__main__":
-#     main()
+        # Calcular el promedio de las tres unidades
+        promedio_unidades = (unidad1 + unidad2 + unidad3) / 3
+
+        nombres.append(nombre)
+        notas_unidades.append([unidad1, unidad2, unidad3])
+        notas_finales.append(promedio_unidades)
+
+    # Crear el gráfico
+    plt.figure(figsize=(12, 6))
+
+    # Número de estudiantes
+    n_estudiantes = len(nombres)
+    
+    # Posiciones de las barras en el eje x
+    x = range(n_estudiantes)
+    
+    # Ancho de las barras
+    width = 0.2
+    
+    # Dibujar las barras para cada unidad y las notas finales
+    for i in range(n_estudiantes):
+        plt.bar(x[i] - width, notas_unidades[i][0], width, color='green', label='Unidad 1' if i == 0 else "")
+        plt.bar(x[i], notas_unidades[i][1], width, color='yellow', label='Unidad 2' if i == 0 else "")
+        plt.bar(x[i] + width, notas_unidades[i][2], width, color='gray', label='Unidad 3' if i == 0 else "")
+        plt.bar(x[i] + 2 * width, notas_finales[i], width, color=colores_finales, label='Nota Final' if i == 0 else "")
+    
+    # Añadir etiquetas de valor sobre cada barra
+    for i in range(n_estudiantes):
+        plt.text(x[i] - width, notas_unidades[i][0], round(notas_unidades[i][0], 2), va='bottom' if notas_unidades[i][0] >= 0 else 'top')
+        plt.text(x[i], notas_unidades[i][1], round(notas_unidades[i][1], 2), va='bottom' if notas_unidades[i][1] >= 0 else 'top')
+        plt.text(x[i] + width, notas_unidades[i][2], round(notas_unidades[i][2], 2), va='bottom' if notas_unidades[i][2] >= 0 else 'top')
+        plt.text(x[i] + 2 * width, notas_finales[i], round(notas_finales[i], 2), va='bottom' if notas_finales[i] >= 0 else 'top')
+
+    # Línea horizontal de referencia para la nota mínima para pasar
+    plt.axhline(y=7, color='gray', linestyle='--', label='Nota mínima para pasar')
+    
+    plt.title(f'Proyección de Notas Finales de los Estudiantes')
+    plt.xlabel('Estudiantes')
+    plt.ylabel('Nota')
+    plt.xticks(range(n_estudiantes), nombres, rotation=90)
+    plt.legend(loc='best')
+    plt.grid(True)
+    plt.tight_layout()
+
+    # Guardar la imagen en un objeto BytesIO en lugar de mostrarla
+    buf = BytesIO()
+    plt.savefig(buf, format='png')
+    buf.seek(0)
+    
+    return buf
