1543f868df9dfbcbde2d7f12d7c4c9ad1746f267
JhostinJATM <146120201+JhostinJATM@users.noreply.github.com>
2024-07-20 17:47:14 -0600

    Mejoras en interfaces (#27)
    Mejora en interfaces

--- client/src/components/inicio_sesion/password/ResetPassword.jsx
+++ client/src/components/inicio_sesion/password/ResetPassword.jsx
@@ -60,49 +60,74 @@ const ResetPassword = () => {
 
   };
 
-  return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-100 dark:bg-gray-900">
-      <div className="bg-white w-full max-w-md p-6 rounded-lg shadow-lg dark:bg-gray-800">
-        <h1 className="text-3xl font-bold text-black mb-4 text-center dark:text-blue-300">Reset Password</h1>
-
-        <form onSubmit={handleSubmit} className="space-y-4">
-          <div>
-            <label htmlFor="password" className="block text-sm font-medium text-gray-700 dark:text-blue-300">
-              New Password:
-            </label>
-            <input
-              id="password"
-              type="password"
-              className="border-gray-300 focus:ring-blue-500 focus:border-blue-500 block w-full sm:text-sm border rounded-md p-2"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              required
-            />
+   return (
+    <div className="w-full h-screen bg-cover bg-center flex items-center justify-center bg-gradient-to-r from-blue-400 to-purple-400 dark:bg-gradient-to-r dark:from-gray-900 dark:to-gray-700">
+      <div className="bg-black bg-opacity-50 rounded-lg shadow-lg max-w-5xl w-full flex">
+        <div className="w-1/2">
+          <img src="../../../public/img/Unl_2.png" alt="Side Visual" className="w-full h-full object-cover rounded-l-lg" />
+        </div>
+        <div className="relative w-1/2 p-12 flex items-center justify-center">
+          <div className="w-full max-w-md">
+            <h1 className="text-3xl font-semibold mb-8 text-white text-center dark:text-green-600">
+              Reset Password
+            </h1>
+            <form onSubmit={handleSubmit} className="space-y-8">
+              <div className="mb-8">
+                <label htmlFor="password" className="block text-1xl font-medium text-white dark:text-gray-300">
+                  New Password:
+                </label>
+                <div className="flex items-center mt-3">
+                  <input
+                    id="password"
+                    type="password"
+                    className={`border rounded-full bg-gray-700 bg-opacity-50 text-white p-4 w-full ${
+                      errorMessage && !password ? "border-red-500" : "border-gray-300" 
+                    } focus:outline-none focus:ring-2 focus:ring-offset-2 ${
+                      errorMessage && !password ? "focus:ring-red-500" : "focus:ring-blue-500"
+                    } transition-colors duration-300`}
+                    placeholder="New Password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    required
+                  />
+                </div>
+              </div>
+              <div className="mb-8">
+                <label htmlFor="confirmPassword" className="block text-1xl font-medium text-white dark:text-gray-300">
+                  Confirm Password:
+                </label>
+                <div className="flex items-center mt-3">
+                  <input
+                    id="confirmPassword"
+                    type="password"
+                    className={`border rounded-full bg-gray-700 bg-opacity-50 text-white p-4 w-full ${
+                      errorMessage && !confirmPassword ? "border-red-500" : "border-gray-300"  
+                    } focus:outline-none focus:ring-2 focus:ring-offset-2 ${
+                      errorMessage && !confirmPassword ? "focus:ring-red-500" : "focus:ring-blue-500"
+                    } transition-colors duration-300`}
+                    placeholder="Confirm Password"
+                    value={confirmPassword}
+                    onChange={(e) => setConfirmPassword(e.target.value)}
+                    required
+                  />
+                </div>
+              </div>
+              {errorMessage && (
+                <span className="text-red-500 text-xl mt-2">{errorMessage}</span>
+              )}
+              <div className="mt-8 border-t-2 border-white dark:border-gray-500"></div>
+              <button
+                type="submit"
+                className="w-full bg-blue-500 text-white p-4 rounded-full hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-700 transition-colors duration-300 dark:bg-green-700 dark:hover:bg-green-800 dark:border-none"
+              >
+                Reset Password
+              </button>
+            </form>
           </div>
-          <div>
-            <label htmlFor="confirmPassword" className="block text-sm font-medium text-gray-700 dark:text-blue-300">
-              Confirm Password:
-            </label>
-            <input
-              id="confirmPassword"
-              type="password"
-              className="border-gray-300 focus:ring-blue-500 focus:border-blue-500 block w-full sm:text-sm border rounded-md p-2"
-              value={confirmPassword}
-              onChange={(e) => setConfirmPassword(e.target.value)}
-              required
-            />
-          </div>
-          {errorMessage && <div className="text-red-500 text-sm mb-4">{errorMessage}</div>}
-          <button
-            type="submit"
-            className="w-full bg-gray-800 text-white p-2 rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-700 transition-colors duration-300 dark:bg-blue-700 dark:hover:bg-blue-800"
-          >
-            Reset Password
-          </button>
-        </form>
+        </div>
       </div>
     </div>
   );
-};
+}
 
 export default ResetPassword;
