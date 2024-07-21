98b73db1082d1149cba03950e29df77d206fba83
JhostinJATM <146120201+JhostinJATM@users.noreply.github.com>
2024-07-21 17:23:04 -0600

    Pull (#37)
    * Mejoras en interfaces
    
    Mejora en interfaces
    
    * Theme oscuro en admin
    
    Implementacion de theme oscuro en paginas de administrador
    
    * Theme oscuro en paginas docente
    
    Implementacion de theme oscuro en paginas de los curso de docente
    
    * Documentacion
    
    Documentacion de academic
    
    * Documentacion catalogos y Passwords
    
    Documentacion en catologos y passwords

--- server/controls/tda/list/utilidades.py
+++ server/controls/tda/list/utilidades.py
@@ -5,6 +5,16 @@ from argon2.exceptions import VerifyMismatchError
 
 #* Funcion de busqueda binaria
 def binary_search(array, target_id):
+    """
+    Realiza una búsqueda binaria en una lista de diccionarios para encontrar un objeto con un ID específico.
+
+    Args:
+        array (dict): Lista de diccionarios que contienen un campo 'id'.
+        target_id (int): El ID que se busca.
+
+    Returns:
+        dict o None: El diccionario con el ID especificado si se encuentra, o None si no se encuentra.
+    """
     left, right = 0, len(array) - 1
     while left <= right:
         mid = (left + right) // 2
@@ -21,10 +31,29 @@ ph = PasswordHasher()
 
 #* Encriptado de contraseñas
 def encrypt_password(password):
+    """
+    Encripta una contraseña utilizando el algoritmo Argon2.
+
+    Args:
+        password (str): La contraseña que se va a encriptar.
+
+    Returns:
+        str: La contraseña encriptada.
+    """
     return ph.hash(password)
 
 #* Verificacion de contraseñas
 def verify_password(password_ingresada, password_encriptada):
+    """
+    Verifica si una contraseña ingresarda coincide con una contraseña encriptada.
+
+    Args:
+        password_ingresada (str): La contraseña ingresada que se va a verificar.
+        password_encriptada (str): La contraseña encriptada con la que se va a comparar.
+
+    Returns:
+        bool: True si la contraseña coincide con la encriptada, False en caso contrario.
+    """
     try:
         valor = ph.verify(password_encriptada, password_ingresada)
         print(f"valor: {valor}")
